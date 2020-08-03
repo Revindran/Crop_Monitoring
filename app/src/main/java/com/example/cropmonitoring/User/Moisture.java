1cee12566f297ffbee5e84d503c3d1890a5d51e5
Raveendran N <ravindran1307@gmail.com>
2020-08-03 16:45:28 +0530

    Update Moisture.java

--- app/src/main/java/com/example/cropmonitoring/User/Moisture.java
+++ app/src/main/java/com/example/cropmonitoring/User/Moisture.java
@@ -3,14 +3,24 @@ package com.example.cropmonitoring.User;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.example.cropmonitoring.R;
 
 public class Moisture extends AppCompatActivity {
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moisture);
+
+        webView = findViewById(R.id.web1);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://192.168.0.121/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
     }
 }
